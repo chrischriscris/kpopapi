7db23a2ab54878d3d5fb8e6f0fa044fd4f6042e2
chrischriscris <18-10892@usb.ve>
2024-06-01 12:53:27 -0400

    fixed migrations to work with psql

--- internal/db/migrations/20240601021607_initial_tables.sql
+++ internal/db/migrations/20240601021607_initial_tables.sql
@@ -15,15 +15,15 @@ CREATE TABLE groups (
     name VARCHAR(255) NOT NULL,
     type CHAR(2) NOT NULL, -- GG, BG, CE (Co-ed)
     debut_date DATE,
-    company_id INTEGER REFERENCES companies(id),
+    company_id INTEGER,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE group_members (
     id SERIAL PRIMARY KEY,
-    group_id INTEGER REFERENCES groups(id),
-    idol_id INTEGER REFERENCES idols(id),
+    group_id INTEGER,
+    idol_id INTEGER,
     since_date DATE,
     until_date DATE,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -35,7 +35,7 @@ CREATE TABLE idols (
     stage_name VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     gender CHAR NOT NULL, -- M, F, O (Other), U (Unknown)
-    idol_info_id INTEGER REFERENCES idol_info(id),
+    idol_info_id INTEGER,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -49,6 +49,13 @@ CREATE TABLE idol_info (
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Foreign key constraints
+ALTER TABLE groups ADD CONSTRAINT fk_company_id FOREIGN KEY (company_id) REFERENCES companies(id);
+ALTER TABLE group_members ADD CONSTRAINT fk_group_id FOREIGN KEY (group_id) REFERENCES groups(id);
+ALTER TABLE group_members ADD CONSTRAINT fk_idol_id FOREIGN KEY (idol_id) REFERENCES idols(id);
+ALTER TABLE idols ADD CONSTRAINT fk_idol_info_id FOREIGN KEY (idol_info_id) REFERENCES idol_info(id);
+
+-- Unique constraints
 ALTER TABLE group_members ADD CONSTRAINT unique_group_idol UNIQUE (group_id, idol_id);
 
 -- +goose StatementEnd
@@ -56,9 +63,9 @@ ALTER TABLE group_members ADD CONSTRAINT unique_group_idol UNIQUE (group_id, ido
 -- +goose Down
 -- +goose StatementBegin
 
-DROP TABLE companies;
+DROP TABLE companies CASCADE;
 
-DROP TABLE groups;
+DROP TABLE groups CASCADE;
 DROP TABLE group_members;
 
 DROP TABLE idols;
--- internal/db/migrations/20240601033224_image_tables.sql
+++ internal/db/migrations/20240601033224_image_tables.sql
@@ -4,7 +4,7 @@
 CREATE TABLE images (
     id SERIAL PRIMARY KEY,
     url TEXT NOT NULL,
-    metadata_id INTEGER NOT NULL REFERENCES image_metadata(id),
+    metadata_id INTEGER NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -20,20 +20,28 @@ CREATE TABLE image_metadata (
 
 CREATE TABLE idol_images (
     id SERIAL PRIMARY KEY,
-    idol_id INTEGER NOT NULL REFERENCES idols(id),
-    image_id INTEGER NOT NULL REFERENCES images(id),
+    idol_id INTEGER NOT NULL,
+    image_id INTEGER NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE group_images (
     id SERIAL PRIMARY KEY,
-    group_id INTEGER NOT NULL REFERENCES groups(id),
-    image_id INTEGER NOT NULL REFERENCES images(id),
+    group_id INTEGER NOT NULL,
+    image_id INTEGER NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Foreign key constraints
+ALTER TABLE images ADD CONSTRAINT fk_metadata_id FOREIGN KEY (metadata_id) REFERENCES image_metadata(id);
+ALTER TABLE idol_images ADD CONSTRAINT fk_idol_id FOREIGN KEY (idol_id) REFERENCES idols(id);
+ALTER TABLE idol_images ADD CONSTRAINT fk_image_id FOREIGN KEY (image_id) REFERENCES images(id);
+ALTER TABLE group_images ADD CONSTRAINT fk_group_id FOREIGN KEY (group_id) REFERENCES groups(id);
+ALTER TABLE group_images ADD CONSTRAINT fk_image_id FOREIGN KEY (image_id) REFERENCES images(id);
+
+-- Unique constraints
 ALTER TABLE idol_images ADD CONSTRAINT unique_idol_image UNIQUE (idol_id, image_id);
 ALTER TABLE group_images ADD CONSTRAINT unique_group_image UNIQUE (group_id, image_id);
 
