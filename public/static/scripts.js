28bb772658c0589825757d8a2f1feb87b2a98650
chrischriscris <18-10892@usb.ve>
2024-08-18 22:56:06 -0400

    kinda something

--- public/static/scripts.js
+++ public/static/scripts.js
@@ -6,3 +6,20 @@ function handleClickOnRandom() {
     1000
   )
 }
+
+/**
+ * Handle the change of the type of addition
+ * @param {HTMLInputElement} type
+ */
+function handleTypeChange(type) {
+  const group = document.querySelector('#group-field')
+  const debut = document.querySelector('#debut-field')
+
+  if (type === 'group') {
+    group.style.display = 'none'
+    debut.style.display = 'block'
+  } else {
+    group.style.display = 'block'
+    debut.style.display = 'none'
+  }
+}
